bc545e31e277e801daeb6eaea534e58615a33067
Mugada Jai Krishna Chaitanya <jaikrishnamugada@gmail.com>
2026-02-20 02:48:54 +0530

    v0.4 - Custom Exceptions and Global Exception Handling

--- src/main/java/com/example/usermanagement/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/usermanagement/exception/GlobalExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
